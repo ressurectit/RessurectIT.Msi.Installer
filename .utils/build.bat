bbc10ff9ba58e730d4fc7f79d0c84fb6f16a266e
kukjevov <kukjevov@gmail.com>
2019-05-15 09:52:52 +0200

    CPT-1857: oprava kompilacie, product code je generovany az ked uz existuje verzia [15m]

--- .utils/build.bat
+++ .utils/build.bat
@@ -1,3 +1,2 @@
-@powershell -ExecutionPolicy Bypass %~dp0updateInstaller.ps1
 @CALL nuget restore %~dp0../RessurectIT.Msi.Installer.sln
 @CALL msbuild %~dp0..\RessurectIT.Msi.Installer.sln /t:Build /p:"Configuration=%~1Installer";"Platform=Any CPU";"JenkinsUserName=%~2";"JenkinsPassword=%~3"

8548915e5e469e8a402a6c52c890617d1687c8d6
kukjevov <kukjevov@gmail.com>
2020-04-02 14:07:52 +0200

    INT: now partialy working installer

--- .utils/build.bat
+++ .utils/build.bat
@@ -1,2 +1,1 @@
-@CALL nuget restore %~dp0../RessurectIT.Msi.Installer.sln
-@CALL msbuild %~dp0..\RessurectIT.Msi.Installer.sln /t:Build /p:"Configuration=%~1Installer";"Platform=Any CPU";"JenkinsUserName=%~2";"JenkinsPassword=%~3"
+@CALL msbuild %~dp0..\RessurectIT.Msi.Installer.sln /t:Build /p:"Configuration=%~1Installer";"Platform=Any CPU"

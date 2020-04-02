6dc40855d0f366f52416b704fc13498cc66712c5
kukjevov <kukjevov@gmail.com>
2020-04-02 22:16:38 +0200

    INT: now working installer gui basic version

--- installer/RessurectIT.Msi.Installer.Installer.CustomActions/CustomAction.cs
+++ installer/RessurectIT.Msi.Installer.Installer.CustomActions/CustomAction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using Microsoft.Deployment.WindowsInstaller;
 
@@ -11,16 +12,12 @@ namespace RessurectIT.Msi.Installer.Installer.CustomActions
     /// </summary>
     public class CustomActions
     {
-        #region constants - shared
+        #region constants
 
         /// <summary>
         /// Name of msi property storing path to config file
         /// </summary>
         private const string ConfigFolder = "INSTALLFOLDER";
-        #endregion
-
-
-        #region constants - UpdateConfig
 
         /// <summary>
         /// Name of UPDATE_JSON_URL property
@@ -46,15 +43,46 @@ namespace RessurectIT.Msi.Installer.Installer.CustomActions
         /// Name of PROGRESS_TYPE property
         /// </summary>
         private const string PropProgressType = "PROGRESS_TYPE";
-        #endregion
-
-
-        #region constants - UpdateConfigPrepareData
 
         /// <summary>
         /// Name of UpdateConfig property
         /// </summary>
         private const string PropUpdateConfig = "UpdateConfig";
+
+        /// <summary>
+        /// Name of default property suffix
+        /// </summary>
+        private const string PropDefaultName = "_DEFAULT";
+
+        /// <summary>
+        /// Default value for unset property
+        /// </summary>
+        private const string DefaultDefault = "__DEFAULT__";
+
+        /// <summary>
+        /// Default value for updates Json Url
+        /// </summary>
+        private const string DefaultUpdatesJsonUrl = "http://localhost:8888/updates.json";
+
+        /// <summary>
+        /// Default value for check Interval
+        /// </summary>
+        private const string DefaultCheckInterval = "300000";
+
+        /// <summary>
+        /// Default value for remote Log Rest Url
+        /// </summary>
+        private const string DefaultRemoteLogRestUrl = "";
+
+        /// <summary>
+        /// Default value for allow Same Version
+        /// </summary>
+        private const string DefaultAllowSameVersion = "";
+
+        /// <summary>
+        /// Default value for progress Type
+        /// </summary>
+        private const string DefaultProgressType = "App";
         #endregion
 
 
@@ -79,6 +107,7 @@ namespace RessurectIT.Msi.Installer.Installer.CustomActions
             session.Log($"Configuration path is '{configPath}'");
             session.Log($"Update json URL: '{updatesJsonUrl}'");
             session.Log($"Check interval: '{checkInterval}'");
+            session.Log($"Progress type: '{progressType}'");
             session.Log($"Remote log rest URL: '{remoteLogRestUrl}'");
 
             try
@@ -134,6 +163,137 @@ namespace RessurectIT.Msi.Installer.Installer.CustomActions
                 session.Log($"UpdateConfig failed '{e.Message}'");
                 
                 return ActionResult.Failure;
+            }
+
+            return ActionResult.Success;
+        }
+        #endregion
+
+
+        #region public methods - ObtainDefaultConfiguration
+
+        /// <summary>
+        /// Obtains default values of configuration properties
+        /// </summary>
+        /// <param name="session">Session of current installer</param>
+        /// <returns>Indication that operation was successful or not</returns>
+        [CustomAction]
+        public static ActionResult ObtainDefaultConfiguration(Session session)
+        {
+            string updatesJsonUrl = session[PropUpdatesJsonUrl];
+            string checkInterval = session[PropCheckInterval];
+            string remoteLogRestUrl = session[PropRemoteLogRestUrl];
+            string allowSameVersion = session[PropAllowSameVersion];
+            string progressType = session[PropProgressType];
+
+            string defaultUpdatesJsonUrl = session[$"{PropUpdatesJsonUrl}-{PropDefaultName}"];
+            string defaultCheckInterval = session[$"{PropCheckInterval}-{PropDefaultName}"];
+            string defaultRemoteLogRestUrl = session[$"{PropRemoteLogRestUrl}-{PropDefaultName}"];
+            string defaultAllowSameVersion = session[$"{PropAllowSameVersion}-{PropDefaultName}"];
+            string defaultProgressType = session[$"{PropProgressType}-{PropDefaultName}"];
+
+            if (string.IsNullOrEmpty(defaultUpdatesJsonUrl))
+            {
+                defaultUpdatesJsonUrl = session[$"{PropUpdatesJsonUrl}-{PropDefaultName}"] = string.IsNullOrEmpty(updatesJsonUrl) ? DefaultDefault : updatesJsonUrl;
+            }
+
+            if (string.IsNullOrEmpty(defaultCheckInterval))
+            {
+                defaultCheckInterval = session[$"{PropCheckInterval}-{PropDefaultName}"] = string.IsNullOrEmpty(checkInterval) ? DefaultDefault : checkInterval;
+            }
+
+            if (string.IsNullOrEmpty(defaultRemoteLogRestUrl))
+            {
+                defaultRemoteLogRestUrl = session[$"{PropRemoteLogRestUrl}-{PropDefaultName}"] = string.IsNullOrEmpty(remoteLogRestUrl) ? DefaultDefault : remoteLogRestUrl;
+            }
+
+            if (string.IsNullOrEmpty(defaultAllowSameVersion))
+            {
+                defaultAllowSameVersion = session[$"{PropAllowSameVersion}-{PropDefaultName}"] = string.IsNullOrEmpty(allowSameVersion) ? DefaultDefault : allowSameVersion;
+            }
+
+            if (string.IsNullOrEmpty(defaultProgressType))
+            {
+                defaultProgressType = session[$"{PropProgressType}-{PropDefaultName}"] = string.IsNullOrEmpty(progressType) ? DefaultDefault : progressType;
+            }
+
+            string defaultConfigContent;
+
+            using(Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("RessurectIT.Msi.Installer.Installer.CustomActions.config.json"))
+            // ReSharper disable once AssignNullToNotNullAttribute
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                defaultConfigContent = reader.ReadToEnd();
+            }
+
+            if (defaultUpdatesJsonUrl == DefaultDefault)
+            {
+                Match match = Regex.Match(defaultConfigContent, @"""UpdatesJsonUrl"":\s?""(?<value>.*?)""", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    session[PropUpdatesJsonUrl] = match.Groups["value"].Value;
+                }
+                else
+                {
+                    session[PropUpdatesJsonUrl] = DefaultUpdatesJsonUrl;                    
+                }
+            }
+
+            if (defaultCheckInterval == DefaultDefault)
+            {
+                Match match = Regex.Match(defaultConfigContent, @"""CheckInterval"":\s?(?<value>\d+)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    session[PropCheckInterval] = match.Groups["value"].Value;
+                }
+                else
+                {
+                    session[PropCheckInterval] = DefaultCheckInterval;                    
+                }
+            }
+
+            if (defaultRemoteLogRestUrl == DefaultDefault)
+            {
+                Match match = Regex.Match(defaultConfigContent, @"""RemoteLogRestUrl"":\s?""(?<value>.*?)""", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    session[PropRemoteLogRestUrl] = match.Groups["value"].Value;
+                }
+                else
+                {
+                    session[PropRemoteLogRestUrl] = DefaultRemoteLogRestUrl;                    
+                }
+            }
+
+            if (defaultAllowSameVersion == DefaultDefault)
+            {
+                Match match = Regex.Match(defaultConfigContent, @"""AllowSameVersion"":\s?(?<value>true|false)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    session[PropAllowSameVersion] = match.Groups["value"].Value.ToLower() == "true" ? "1" : "";
+                }
+                else
+                {
+                    session[PropAllowSameVersion] = DefaultAllowSameVersion;                    
+                }
+            }
+
+            if (defaultProgressType == DefaultDefault)
+            {
+                Match match = Regex.Match(defaultConfigContent, @"""Progress"":\s?""(?<value>App|None|MsiExec)""", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    session[PropProgressType] = match.Groups["value"].Value;
+                }
+                else
+                {
+                    session[PropProgressType] = DefaultProgressType;                    
+                }
             }
 
             return ActionResult.Success;
